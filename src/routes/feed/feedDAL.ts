07b01f447be8c4298c0ead6e61178a17eb075f2b
shivali28 <shivali.pawar06@gmail.com>
2020-06-02 12:05:16 +0530

    Get feeds api corrected for both specific and all topics.

--- src/routes/feed/feedDAL.ts
+++ src/routes/feed/feedDAL.ts
@@ -1,109 +1,24 @@
-import { client } from "../../config/db";
+import { client } from '../../config/db';
 
 export const getAllFeeds = async (req: any, res: any) => {
-    console.log("Topic name " + JSON.stringify(req.query.topicid));
+    console.log("Topic id " + JSON.stringify(req.query.topicid));
     try {
-        let finalResponse: Array<responseFeed> = [];
         if (!req.query.topicid) {
             let query = `SELECT topic_id, topic_name, username FROM topic`;
             let result = await client.query(query);
-            console.log("All Topics" + JSON.stringify(result));
+            console.log("All Topics" + JSON.stringify(result.rows));
             let topics = result.rows;
-            topics.forEach(async (topic) => {
-                let query = `SELECT question_id, question_name, username from question where question.topic_id = $1`;
-                let values = [topic.topic_id];
-                let result = await client.query(query, values);
-                console.log("All question for given topic " + JSON.stringify(result));
-                let questionList = result.rows;
-                questionList.forEach(async (question) => {
-                    let query = `SELECT answer_id, answer, username from answer where answer.question_id = $1`;
-                    let values = [question.question_id];
-                    let result = await client.query(query, values);
-                    console.log("All answers for topic " + JSON.stringify(result));
-                    let answerList = result.rows;
-                    answerList.forEach((answer) => {
-                        let topicWithQuesAns = {
-                            topics: {
-                                name: topic.topic_name,
-                                user: topic.username,
-                                questions: [
-                                    {
-                                        name: question.question_name,
-                                        user: question.username,
-                                        answer: [
-                                            {
-                                                name: answer.answer,
-                                                user: answer.username,
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
-                        }
-                        finalResponse.push(topicWithQuesAns);
-                    })
-                    res.status(200).json({ "feeds": finalResponse });
-                })
+            FetchDataForTopics(topics).then(result => {
+                res.status(200).json({ "feed": { "topics": result } });
             })
         } else {
             let query = `SELECT topic_id, topic_name, username FROM topic WHERE topic.topic_id = $1`;
             let values = [req.query.topicid];
             let result = await client.query(query, values);
-            console.log("Topic for give id is " + JSON.stringify(result));
-            if (result.rowCount > 0) {
-                let topics = result.rows;
-                topics.forEach(async (topic) => {
-                    let query = `SELECT question_id, question_name, username from question where question.topic_id = $1`;
-                    let values = [topic.topic_id];
-
-                    let result = await client.query(query, values);
-                    console.log("All question for given topic " + JSON.stringify(result));
-                    if (result.rowCount > 0) {
-                        let questionList = result.rows;
-                        questionList.forEach(async (question) => {
-                            let query = `SELECT answer_id, answer, username from answer where answer.question_id = $1`;
-                            let values = [question.question_id];
-
-                            let result = await client.query(query, values);
-                            console.log("All answers for topic " + JSON.stringify(result));
-                            if (result.rowCount > 0) {
-                                let answerList = result.rows;
-                                answerList.forEach((answer) => {
-                                    let topicWithQuesAns = {
-                                        topics: {
-                                            name: topic.topic_name,
-                                            user: topic.username,
-                                            questions: [
-                                                {
-                                                    name: question.question_name,
-                                                    user: question.username,
-                                                    answer: [
-                                                        {
-                                                            name: answer.answer,
-                                                            user: answer.username,
-                                                        }
-                                                    ]
-                                                }
-                                            ]
-                                        }
-                                    }
-                                    finalResponse.push(topicWithQuesAns);
-                                })
-                                res.status(200).json({ "feeds": finalResponse });
-                            } else {
-                                console.log("No answers for topic");
-                                res.status(404).send({ "message": "No feeds available" });
-                            }
-                        })
-                    } else {
-                        console.log("No question for topic");
-                        res.status(404).send({ "message": "No feeds available" });
-                    }
-
-                })
-            } else {
-                res.status(404).send({ "message": "No feeds available" });
-            }
+            console.log("Topic for give id is " + JSON.stringify(result.rows));
+            FetchDataForTopics(result.rows).then(result => {
+                res.status(200).json({ "feed": { "topics": result } });
+            })
         }
     } catch (error) {
         console.log("Error fetching feeds " + JSON.stringify(error));
@@ -111,17 +26,53 @@ export const getAllFeeds = async (req: any, res: any) => {
     }
 }
 
-type responseFeed = {
-    topics: {
-        name: string;
-        user: string;
-        questions: {
-            name: string;
-            user: string;
-            answer: {
-                name: string;
-                user: string;
-            }[];
-        }[];
-    }
+function FetchDataForTopics(topics: any[]) {
+    return new Promise((res, rej) => {
+        const questionsPromises = topics.map((topic) => {
+            let query = `SELECT question_id, question_name, username from question where question.topic_id = $1`;
+            let values = [topic.topic_id];
+            return client.query(query, values);
+        });
+
+        Promise.all(questionsPromises).then(results => {
+            const allResultPromises = results.map(r => {
+                return getAllAnswersForAllQuestions(r.rows)
+            })
+
+            Promise.all(allResultPromises).then(results => {
+                const topicResults = topics.map((t, index) => {
+                    return {
+                        name: t.topic_name,
+                        user: t.username,
+                        questions: results[index]
+                    }
+                })
+                res(topicResults)
+            })
+        })
+    })
+
 }
+
+function getAllAnswersForAllQuestions(questions: any[]) {
+    return new Promise((resolve, reject) => {
+        const promises = questions.map((question) => {
+            let query = `SELECT answer_id, answer, username from answer where answer.question_id = $1`;
+            let values = [question.question_id];
+            return client.query(query, values);
+        });
+
+        Promise.all(promises).then(results => {
+            const allAnswers = results.map(r => r.rows)
+            const res = questions.map((q, index) => {
+                return {
+                    name: q.question_name,
+                    user: q.username,
+                    answers: allAnswers[index]
+                }
+            })
+            resolve(res)
+        })
+    })
+}
+
